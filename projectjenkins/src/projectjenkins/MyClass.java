9aaa431253cf562ba9ae1122e1d757c987a59d8a
chandu <chandubanoth980@gmail.com>
2021-10-10 20:56:24 +0530

    first commit

---
+++ projectjenkins/src/projectjenkins/MyClass.java
@@ -0,0 +1,13 @@
+package projectjenkins;
+
+public class MyClass {
+
+	public static void main(String args[]) {
+		int a=9;
+		int b=3;
+		int c=a+b;
+		System.out.println("The result is "+c);
+
+	}
+
+}
